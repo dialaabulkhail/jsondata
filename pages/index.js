118edb4789e11118876ff77f3fe3a48a78904c05
dialaabulkhail <diala.sh.98@gmail.com>
2023-05-30 07:33:23 +0000

    added json

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Test from './components/Test'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
    <div>
-    hello world
+    <Test />
    </div>
   )
 }
